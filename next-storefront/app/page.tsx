897677dd2b62a1df535b7f87a9b28e96958a55d4
Luiza Petrinina <petrinina95@gmail.com>
2026-03-19 12:04:26 +0200

    Install the Shadcn library
    -Add the necessary components

--- next-storefront/app/page.tsx
+++ next-storefront/app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 function HomePage() {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      <div>Home Page</div>
+      <Button variant='outline' size='default' className='capitalize m-8'>
+        click me
+      </Button>
+    </div>
+  );
 }
 
 export default HomePage;
