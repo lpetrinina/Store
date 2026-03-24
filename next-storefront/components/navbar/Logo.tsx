31c62308df7bbec037c7cd805f47ca60a86e49fa
Luiza Petrinina <petrinina95@gmail.com>
2026-03-24 14:10:18 +0200

    Change the Logo

--- next-storefront/components/navbar/Logo.tsx
+++ next-storefront/components/navbar/Logo.tsx
@@ -1,20 +1,18 @@
 import Link from "next/link";
-import { Button } from "../ui/button";
-import { BiHomeHeart } from "react-icons/bi";
-import { HiHome } from "react-icons/hi2";
-import { TbHeartCode } from "react-icons/tb";
-import { VscCode } from "react-icons/vsc";
+import Image from "next/image";
+
+import icon from "@/app/icon.png";
 
 function Logo() {
   return (
-    <Button asChild size='icon-lg'>
-      <Link href='/'>
-        <BiHomeHeart />
-        {/* <HiHome /> */}
-        {/* <TbHeartCode /> */}
-        {/* <VscCode /> */}
-      </Link>
-    </Button>
+    <Link href='/' className='flex flex-col items-center gap-1'>
+      <div className='w-8 sm:w-10 aspect-square'>
+        <Image src={icon} alt='logo' className='object-cover' />
+      </div>
+      <span className='text-[8px] sm:text-[9px] uppercase font-bold text-secondary-foreground/70 tracking-widest'>
+        Nordik home
+      </span>
+    </Link>
   );
 }
 
