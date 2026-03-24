beacbd9504751324dbf2c732a87e3aa51f19fa87
Luiza Petrinina <petrinina95@gmail.com>
2026-03-24 23:55:42 +0200

    Build the Products page
    - Add a loading

--- next-storefront/app/products/page.tsx
+++ next-storefront/app/products/page.tsx
@@ -1,5 +1,16 @@
-function ProductsPage() {
-  return <div>Products Page</div>;
+import ProductContainer from "@/components/products/ProductContainer";
+
+async function ProductsPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ layout?: string; search?: string }>;
+}) {
+  const searchParamsData = await searchParams;
+
+  const layout = searchParamsData.layout || "grid";
+  const search = searchParamsData.search || "";
+
+  return <ProductContainer layout={layout} search={search} />;
 }
 
 export default ProductsPage;
