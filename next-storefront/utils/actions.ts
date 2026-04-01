b1f39c438593e5116c1433b7442d06692700c912
Luiza Petrinina <petrinina95@gmail.com>
2026-04-01 15:56:47 +0300

    Implement a create product functionality
    - Add getAuthUser helper function to check if there is user
    - Add createProductAction server action

--- next-storefront/utils/actions.ts
+++ next-storefront/utils/actions.ts
@@ -1,5 +1,21 @@
+'use server';
+
 import prisma from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+
+async function getAuthUser() {
+    const user = await currentUser();
+
+    if (!user) {
+        throw new Error('You must be logged in to access this route');
+    }
+
+    return user;
+}
+
+
+//  GET product/s
 
 export async function fetchFeaturedProducts() {
     const products = await prisma.product.findMany({
@@ -40,5 +56,39 @@ export async function fetchSingleProduct(productId: string) {
     }
 
     return product;
+
+}
+
+// CREATE product
+
+export async function createProductAction(prevState: any, formData: FormData): Promise<{ error: null | string, success: boolean }> {
+
+    const user = await getAuthUser();
+
+    try {
+        const name = formData.get('name') as string;
+        const company = formData.get('company') as string;
+        const price = Number(formData.get('price') as string);
+        const image = formData.get('image') as File;
+        const description = formData.get('description') as string;
+        const featured = Boolean(formData.get('featured') as string);
+
+        await prisma.product.create({
+            data: {
+                name,
+                company,
+                price,
+                image: '/images/product-1.jpg',
+                description,
+                featured,
+                clerkId: user.id
+            }
+        })
+
+        return { error: null, success: true }
+    } catch (err) {
+
+        return { error: err instanceof Error ? err.message : 'An error occurred', success: false }
+    }
 
 }
