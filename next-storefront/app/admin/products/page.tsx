0a79a8d989550f00e5b8eca3b6326378c88b11b0
Luiza Petrinina <petrinina95@gmail.com>
2026-04-03 22:53:21 +0300

    Build the AdminProducts page

--- next-storefront/app/admin/products/page.tsx
+++ next-storefront/app/admin/products/page.tsx
@@ -1,6 +1,17 @@
-import EmptyList from "@/components/global/EmptyList";
-import { Table, TableCaption } from "@/components/ui/table";
+import Link from "next/link";
+
 import { fetchAdminProducts } from "@/utils/actions";
+import { formatCurrency } from "@/utils/format";
+import EmptyList from "@/components/global/EmptyList";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 async function AdminProductsPage() {
   const products = await fetchAdminProducts();
@@ -15,6 +26,37 @@ async function AdminProductsPage() {
         <TableCaption className='capitalize'>
           total products: {products.length}
         </TableCaption>
+
+        <TableHeader>
+          <TableRow>
+            <TableHead className='capitalize'>product name</TableHead>
+            <TableHead className='capitalize'>company</TableHead>
+            <TableHead className='capitalize'>price</TableHead>
+            <TableHead className='capitalize'>actions</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {products.map((product) => {
+            const { id: productId, name, company, price } = product;
+
+            return (
+              <TableRow key={productId}>
+                <TableCell>
+                  <Link
+                    href={`/products/${productId}`}
+                    className='text-muted-foreground tracking-wide capitalize'
+                  >
+                    {name}
+                  </Link>
+                </TableCell>
+
+                <TableCell>{company}</TableCell>
+                <TableCell>{formatCurrency(price)}</TableCell>
+              </TableRow>
+            );
+          })}
+        </TableBody>
       </Table>
     </section>
   );
